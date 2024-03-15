ce9bcc58ca8ed7cab055190b7c2ba3af290dcda6
daawnee <Daniel_Kiss@mckinsey.com>
2024-03-15 18:07:30 +0100

    App restructuring

---
+++ common/params.py
@@ -0,0 +1,33 @@
+import azure.functions as func
+from functools import cache
+
+from typing import Any
+
+
+class Params:
+    def __init__(self, req: func.HttpRequest) -> None:
+        self.__headers = req.headers
+        self.__get = req.params
+        try:
+            self.__body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            self.__body = {}
+
+    @cache
+    def __getitem__(self, index: str) -> Any:
+        if value := self.__headers.get(index) is not None:
+            return value
+
+        if value := self.__get.get(index) is not None:
+            return value
+
+        value = self.__body
+
+        for i in index.split("."):
+            value = value.get(i)
+            if value is None:
+                break
+
+        return value
---
+++ endpoints/calculate.py
@@ -0,0 +1,22 @@
+import azure.functions as func
+import logging
+
+from ..common import Params
+
+
+def calculate(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info("Endpoint calculate processed a request.")
+
+    params = Params(req)
+
+    name = params["name"]
+
+    if name:
+        return func.HttpResponse(
+            f"Hello, {name}. This HTTP triggered function executed successfully."
+        )
+    else:
+        return func.HttpResponse(
+            "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+            status_code=200,
+        )
